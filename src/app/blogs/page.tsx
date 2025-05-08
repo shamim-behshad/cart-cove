ce11140501ce3129ca2795b4f228aaca134301f4
shamim-behshad <shamim.behshad@gmail.com>
2025-05-08 13:16:35 +0330

    feat: articlle component

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,20 +1,20 @@
-import Container from '@/components/Container'
-import React from 'react'
+import Articles from "@/components/Articles";
+import Container from "@/components/Container";
+import React from "react";
 
 function Blogs() {
   return (
     <Container>
-      <div className='grid grid-cols-4 gap-4'>
-          <div className='bg-black h-16'></div>
-          <div className='bg-black h-16'></div>
-          <div className='bg-black h-16'></div>
-          <div className='bg-black h-16'></div>
-          <div className='bg-black h-16'></div>
-          <div className='bg-black h-16'></div>
-          <div className='bg-black h-16'></div>
+      <div className="grid grid-cols-4 gap-4 py-16">
+        <Articles />
+        <Articles />
+        <Articles />
+        <Articles />
+        <Articles />
+        <Articles />
       </div>
     </Container>
-  )
+  );
 }
 
-export default Blogs
+export default Blogs;
---
+++ src/components/Articles.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Articles() {
+  return (
+    <div className="shadow p-4">
+      <h2>Title</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem saepe
+        amet ad illum recusandae est corrupti ipsam ex vel inventore!
+      </p>
+    </div>
+  );
+}
+
+export default Articles;
