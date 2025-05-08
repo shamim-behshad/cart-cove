ee320d9bcb972104e1c9e55ea7ec5be365128f11
shamim-behshad <shamim.behshad@gmail.com>
2025-05-08 13:35:24 +0330

    feat: api call for blogs

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -2,21 +2,24 @@ import Articles from "@/components/Articles";
 import Container from "@/components/Container";
 import React from "react";
 
- function Blogs() {
-  // const results = await fetch('http://localhost:3001/articles');
-  // const data = await results.json();
+export interface ArticlesType {
+  id: number;
+  title: string;
+  description: string;
+}
 
-  // console.log(data)
+async function Blogs() {
+  const results = await fetch("http://localhost:3001/articles");
+  const data = (await results.json()) as ArticlesType[];
+
+  console.log(data);
 
   return (
     <Container>
       <div className="grid grid-cols-4 gap-4 py-16">
-        <Articles />
-        <Articles />
-        <Articles />
-        <Articles />
-        <Articles />
-        <Articles />
+        {data.map((item) => (
+          <Articles />
+        ))}
       </div>
     </Container>
   );
