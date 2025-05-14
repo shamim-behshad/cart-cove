9b6ee5a7e99fae2408b0a859d5ec4c26b95fc57a
shamim-behshad <shamim.behshad@gmail.com>
2025-05-14 11:45:09 +0330

    fix: fixing navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,33 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 function Navbar() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    {
+      href: "/",
+      title: "Home",
+    },
+    {
+      href: "/store",
+      title: "Store",
+    },
+  ];
+
   return (
     <nav className="shadow p-4">
-      <Link className="pr-4" href={"./"}>Home</Link>
-      <Link className="pr-4" href={"./store"}>Store</Link>
+      {navLinks.map((item) => (
+        <Link
+          key={item.href}
+          className={`pr-4 ${pathname === item.href ? "text-sky-500" : ""}`}
+          href={item.href}
+        >
+          {item.title}
+        </Link>
+      ))}
     </nav>
   );
 }
