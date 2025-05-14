36e2625f73c452cef0ece991d05d4d264eb61cb5
shamim-behshad <shamim.behshad@gmail.com>
2025-05-14 10:33:16 +0330

    feat: create blog route

---
+++ src/app/create-blog/page.tsx
@@ -0,0 +1,15 @@
+import { Component } from "react"
+
+
+function CreateBlog() {
+  return (
+    <Component>
+        <label>Title</label>
+        <input type="text" />
+        <label>Description</label>
+        <textarea> </textarea>
+    </Component>
+  )
+}
+
+export default CreateBlog
